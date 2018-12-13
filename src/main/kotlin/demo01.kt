d0e931a7c2d15960665bc43ac016299995d5bb81
TestSmirk <d.kc@qq.com>
2018-12-13 16:36:07 +0800

    cancelAndJoin

--- src/main/kotlin/demo01.kt
+++ src/main/kotlin/demo01.kt
@@ -1,14 +1,21 @@
 import kotlinx.coroutines.*
 
-fun main(args: Array<String>) = runBlocking{
-
-    GlobalScope.launch {
-        repeat(1000) {
-            println("I an sleeping $it")
-            delay(500L)
-
+fun main(args: Array<String>) = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (i < 5) {
+            //print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("I`m sleeping $${i++}")
+                nextPrintTime += 500L
+            }
         }
     }
     delay(1300L)
+    println("main: I`m tired of waiting~")
+    job.cancelAndJoin()
+    println("main:Now I can quit")
 
 }
