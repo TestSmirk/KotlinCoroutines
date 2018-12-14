26824dc609fcf6763f43960b65fcf987fd6c9214
TestSmirk <d.kc@qq.com>
2018-12-14 10:32:46 +0800

    channel close

--- src/main/kotlin/demo01.kt
+++ src/main/kotlin/demo01.kt
@@ -3,11 +3,10 @@ import kotlinx.coroutines.channels.Channel
 
 fun main(args: Array<String>) = runBlocking {
     val channel = Channel<Int>()
-
     launch {
         for (x in 1..5) channel.send(x * x)
+        channel.close()
     }
-    repeat(5) { println(channel.receive()) }
+    for (y in channel) println(y)
     println("Done!")
-
 }
