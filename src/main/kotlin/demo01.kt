49ddb035668c97013dd21a0cffa8314f38eb63e2
TestSmirk <d.kc@qq.com>
2018-12-14 10:26:31 +0800

    withTimeoutOrNull

--- src/main/kotlin/demo01.kt
+++ src/main/kotlin/demo01.kt
@@ -1,23 +1,12 @@
 import kotlinx.coroutines.*
 
 fun main(args: Array<String>) = runBlocking {
-    val job = launch {
-        try {
-            repeat(1000)
-            {
-                println("I am sleeping $it")
-                delay(500L)
-
-            }
-
-        } finally {
-            println("finally")
+    val result = withTimeoutOrNull(1300L) {
+        repeat(1000) {
+            println("I am sleeping $it")
+            delay(500L)
         }
+        "Done"
     }
-    delay(1300L)
-    println("mai'nL I am tired of waiting!")
-    job.cancelAndJoin()
-    println("main now I am quit")
-
-
+    println("Result is $result")
 }
